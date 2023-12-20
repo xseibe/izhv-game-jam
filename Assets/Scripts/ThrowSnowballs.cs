8e5cc5a4983b62c2df1121dff0d7f791c1600399
xseibe <xseibe00@vutbr.cz>
2023-12-20 21:37:27 +0100

    Implementation of throwable snowballs

--- Assets/Scripts/ThrowSnowballs.cs
+++ Assets/Scripts/ThrowSnowballs.cs
@@ -1,9 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ThrowSnowballs : MonoBehaviour
 {
+    [Header("References")]
+    [SerializeField] Transform Camera;
+    [SerializeField] GameObject SnowballPrefab;
+
+    [Header("Settings")]
+    [SerializeField] float ThrowCooldown;
+    [SerializeField] float ThrowForce;
+    [SerializeField] float ThrowUpForce;
+
+    private float throwTimer = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,26 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.DrawRay(Camera.position, Camera.forward, Color.white);
+        throwTimer -= Time.deltaTime;
+
+        // If the cooldown passed
+        if (throwTimer < 0f && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Throw();
+        }
     }
+
+    private void Throw()
+    {
+        // Spawn the snowball
+        GameObject snowball = Instantiate(SnowballPrefab, Camera.position, Camera.rotation);
+
+        Rigidbody snowballRb = snowball.GetComponent<Rigidbody>();
+
+        // Adding force to the snowball
+        snowballRb.AddForce(Camera.forward * ThrowForce + transform.up * ThrowUpForce, ForceMode.Impulse);
+
+        throwTimer = ThrowCooldown;
+    }    
 }
